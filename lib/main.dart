c7ff2f8beb8e9d9d2822fccf8dfe1d39f0ece1f3
luticakep <kaylasorayadj@gmail.com>
2024-12-04 20:34:27 +0700

    change to LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:beauty_from_the_seoul_mobile/events/screens/event_list.dart';
+import 'package:beauty_from_the_seoul_mobile/authentication/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
           ).copyWith(secondary: Colors.blue[900]),
         ),
-        home: EventPage(),
+        home: LoginPage(),
       ),
     );
   }
